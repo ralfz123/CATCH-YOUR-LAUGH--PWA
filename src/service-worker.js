36fc31c1607e841db02885acf66c2c8e9330ff81
ralfz123 <ralf_zonneveld@hotmail.com>
2021-03-29 21:21:30 +0200

    done service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,71 +1,75 @@
-const CORE_CACHE_NAME = 'site-static';
-const CORE_ASSETS = [
-  // '/favourites',
+const CORE_CACHE_VERSION = 'v4';
+const urlsToCache = [
   '/offline',
   'css/main.css',
-  // 'https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600&display=swap',
-  // 'https://fonts.gstatic.com/s/quicksand/v22/6xKtdSZaM9iE8KbpRA_hJFQNYuDyP7bh.woff2',
+  'https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600&display=swap%27',
+  'icons/offline_logo.svg',
 ];
-// const CORE_WHITELIST = ['/favourites'];
 
 self.addEventListener('install', (event) => {
-  console.log('install');
+  console.log('installing service worker');
 
-  // Pre caching assets - Does not finish install event until this promise below is resolved
   event.waitUntil(
-    caches
-      .open(CORE_CACHE_NAME)
-      .then((cache) => cache.addAll(CORE_ASSETS))
-      .then(self.skipWaiting())
+    caches.open(CORE_CACHE_VERSION).then(function (cache) {
+      return cache.addAll(urlsToCache).then(() => self.skipWaiting());
+    })
   );
 });
 
 self.addEventListener('activate', (event) => {
-  // console.log('activate');
-  // if er is --> update, dan eruit
-  // check (if) naam overeen komt met en met id
-  // online - fav no cache
-  // offline - haal uit cache - update! // config --  er is nieuwe versie
-  // event.waitUntil(clients.claim());
+  console.log('active');
+  event.waitUntil(clients.claim());
 });
 
+// when worker fetches
 self.addEventListener('fetch', (event) => {
-  // console.log('fetch', event.request);
-
-  // Saves visited pages/content and send offline page when internet is broken --> has to send offline when on home?
   if (
-    event.request.mode === 'navigate' ||
-    (event.request.method === 'GET' &&
-      event.request.headers.get('accept').includes('text/html'))
+    event.request.method === 'GET' &&
+    event.request.destination === 'document'
   ) {
     event.respondWith(
-      caches.open(CORE_CACHE_NAME).then((cache) => {
-        return (
-          cache
-            // if req is in cache, return with response
-            .match(event.request)
-            .then((response) => {
-              if (response) {
-                return response;
-              }
-              // save req to cache
-              return fetch(event.request).then((response) => {
-                cache.put(event.request, response.clone());
-                return response;
-              });
-            })
-            .catch((err) => {
-              return caches
-                .open(CORE_CACHE_NAME)
-                .then((cache) => cache.match('/offline'));
-            })
-        );
+      fetch(event.request).catch(() => {
+        return caches
+          .open(CORE_CACHE_VERSION)
+          .then((cache) => cache.match('/offline'));
       })
-  // '/' & '/favourites' | when online --> serve from server, but update cache (/favourites)
-  // '/' & '/favourites' | when offline --> serve from cache
-   
-      );
+    );
+  } else if (
+    event.request.method === 'GET' &&
+    event.request.destination === 'style'
+  ) {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches
+          .open(CORE_CACHE_VERSION)
+          .then((cache) => cache.match('css/main.css'));
+      })
+    );
+  } else if (
+    event.request.method === 'GET' &&
+    event.request.destination === 'font'
+  ) {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches
+          .open(CORE_CACHE_VERSION)
+          .then((cache) =>
+            cache.match(
+              'https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600&display=swap%27'
+            )
+          );
+      })
+    );
+  } else if (
+    event.request.method === 'GET' &&
+    event.request.destination === 'image'
+  ) {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches
+          .open(CORE_CACHE_VERSION)
+          .then((cache) => cache.match('icons/offline_logo.svg'));
+      })
+    );
   }
 });
-
-// Fetch - source: https://deanhume.com/create-a-really-really-simple-offline-page-using-service-workers/
